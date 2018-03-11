8056e907b942497b20b361c1d194c61e1ab28ee0
Javier Navarro <jnavarros@psl.com.co>
2018-03-11 15:44:58 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', text => {
+    return text.toUpperCase();
+});
+
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    const log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', error => {
+        if (error) console.log('Unable to append to server log');
+    });
+    next();
+});
+
+app.use((req, res) => {
+    res.render('example.hbs', {});
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Welcome Page',
+        welcomeMessage: 'Hola Javier'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });    
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Page unavailable'
+    });    
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
