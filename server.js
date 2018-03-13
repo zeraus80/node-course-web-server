6e4444e98f09c180ada493ac89ccf1fd7f5b09a6
Javier Navarro <jnavarros@psl.com.co>
2018-03-12 21:49:08 -0500

    Fixing redirect

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res) => {
+/* app.use((req, res) => {
     res.render('example.hbs', {});
-});
+}); */
 
 app.use(express.static(__dirname + '/public'));
 
