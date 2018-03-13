52e000138986f52ebe1490748142cb483a879d50
Javier Navarro <jnavarros@psl.com.co>
2018-03-13 17:54:29 -0500

    Add portfolio of projects page

--- server.js
+++ server.js
@@ -44,6 +44,12 @@ app.get('/about', (req, res) => {
     });    
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page'
+    });    
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Page unavailable'
